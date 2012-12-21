6486d826bd103ed5181d4d4c5a56fbb7c6a8628b
Saad Arif <saadarif006@gmail.com>
2012-12-21 13:46:16 +0000

    Added a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+	def home
+	end
 
-  def help
-  end
+	def help
+	end
 
-  def about
-  end
+	def about
+	end
+
+	def contact
+	end
 end
