c3daad026d5b1e6147602dcda2a26cd898effe62
Saad Arif <saadarif006@gmail.com>
2013-01-02 20:35:00 +0000

    Finish sign in - No Cucumber

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 SampleApp::Application.routes.draw do
   resources :users
 
-  root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destory]
 
   match '/signup',  to: 'users#new'
+
+  match '/signin', to: 'sessions#new'
+
+  match '/signout', to: 'sessions#destroy', via: :delete
   
+  root to: 'static_pages#home'
+
   match '/help',    to: "static_pages#help"
 
   match '/about',   to:"static_pages#about"
