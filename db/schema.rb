b009c338d75a15751d54f0716c01aa2fcdd78357
Konstantin Devyatov <konstantin@konstantin-Aspire-5920G.(none)>
2013-01-10 18:35:39 +0000

    Added sport column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102183723) do
+ActiveRecord::Schema.define(:version => 20130110181536) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130102183723) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "sport"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
