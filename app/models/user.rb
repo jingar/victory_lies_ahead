59efe2118d3fba256965e8d072aa2bb77f158991
Konstantin Devyatov <konstantin@konstantin-Aspire-5920G.(none)>
2013-01-11 14:21:11 +0000

    Added radio buttons that submit 'on'

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :sport
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  validates :sport, presence: true
 
   private
     def create_remember_token
