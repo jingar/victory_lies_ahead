118831ec0fe0586f977a91d310dd2e313155182a
Konstantin Devyatov <konstantin@konstantin-Aspire-5920G.(none)>
2013-01-12 17:47:50 +0000

    fixed wattball model and gitignore

--- app/models/wattball.rb
+++ app/models/wattball.rb
@@ -2,8 +2,8 @@ class Wattball < ActiveRecord::Base
   attr_accessible :team_name, :first_name, :last_name
   belongs_to :user
 
- 	validates :team_name, presence: true, uniqueness: { case_sensitive: false }
- 	validates :first_name, presence: true
+ 	validates :team_name, presence: true
+	validates :first_name, presence: true
  	validates :last_name, presence: true
 
 end
