e4fde9ec5192d8d1c9460759687d442b36d6035d
Konstantin Devyatov <kdnalus@gmail.com>
2013-01-19 17:20:40 +0000

    added validation to staff model

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -14,5 +14,22 @@
 #
 
 class Staff < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :user_name, :password, :password_confirmation, :type
+    attr_accessible :first_name, :last_name, :user_name,
+    :password, :password_confirmation, :type
+    has_secure_password
+
+    before_save { |staff| staff.user_name = user_name.downcase }
+    before_save :create_remember_token
+
+    validates :first_name, presence: true, length: {maximum: 50}
+    validates :last_name, presence: true, length: Pmaximum: 50 }
+    validates :email, presence: true, uniqueness: { case_sensetive: false }
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_conformation, presence: true
+    validates :type, presence: true
+
+    private
+	def create_remember-token
+	    self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
