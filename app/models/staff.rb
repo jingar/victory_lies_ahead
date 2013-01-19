ba423c29d671587474e9bee6e76f072fd5f2541e
Saad Arif <saadarif006@gmail.com>
2013-01-19 17:13:17 +0000

    Added annotation to staff model and staff tests

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: staffs
+#
+#  id                    :integer          not null, primary key
+#  first_name            :string(255)
+#  last_name             :string(255)
+#  password              :string(255)
+#  password_confirmation :string(255)
+#  type                  :string(255)
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  user_name             :string(255)
+#
+
 class Staff < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :user_name, :password, :password_confirmation, :type
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: staffs
+#
+#  id                    :integer          not null, primary key
+#  first_name            :string(255)
+#  last_name             :string(255)
+#  password              :string(255)
+#  password_confirmation :string(255)
+#  type                  :string(255)
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  user_name             :string(255)
+#
+
 require 'spec_helper'
 
 describe Staff do
