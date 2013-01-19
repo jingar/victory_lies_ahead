199a176e47bab6cc45bad040651a4f3bf9934604
Konstantin Devyatov <kdnalus@gmail.com>
2013-01-19 23:17:21 +0000

    staff rspec error fixing

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -24,11 +24,11 @@ class Staff < ActiveRecord::Base
     validates :first_name, presence: true, length: {maximum: 50}
     validates :last_name, presence: true, length: { maximum: 50 }
     validates :password, presence: true, length: { minimum: 6 }
-    validates :password_conformation, presence: true
+    validates :password_confirmation, presence: true
     validates :type, presence: true
 
 	private
 	def create_remember_token
-		self.remember_token = SecureRandom.urlsafe_base64
+	    self.remember_token = SecureRandom.urlsafe_base64
 	end
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -16,93 +16,92 @@
 require 'spec_helper'
 
 describe Staff do
- let(:found_staff) { Staff.find_by_first_name(@staff.email) }
-  before do
-    @staff = Staff.new(first_name: "John", last_name: "Smith", 
-    password: "foobar", password_confirmation: "foobar")
-  end
-
-  subject { @staff }
-
-	it { should respond_to(:first_name) }
-  it { should respond_to(:last_name) }
-  it { should respond_to(:password_digest) }
- 	it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-	it { should respond_to(:remember_token) }
-	it { should respond_to(:authenticate) }
-  
-
-  it { should be_valid }
-
-  describe "when first name is not present" do
-    before { @staff.first_name = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when last name is not present" do
-    before { @staff.last_name = " " }
-    it { should_not be_valid }
-  end
-
-
-   describe "when first name is too long" do
-    before { @staff.first_name = "a" * 51 }
-    it { should_not be_valid }
-  end
-
-   describe "when last name is too long" do
-    before { @staff.last_name = "a" * 51 }
-    it { should_not be_valid }
-  end
-
-  describe "when user name is already taken" do
+    let(:found_staff) { Staff.find_by_first_name(@staff.first_name) }
     before do
-      staff_with_same_user_name = @staff.dup
-      staff_with_same_user_name.save
+	@staff = Staff.new(first_name: "John", last_name: "Smith", 
+	password: "foobar", password_confirmation: "foobar")
     end
 
-    it { should_not be_valid }
-  end
+    subject { @staff }
 
-  describe "when password is not present" do
-    before { @staff.password = @staff.password_confirmation = " " }
-    it { should_not be_valid }
-  end
+    it { should respond_to(:first_name) }
+    it { should respond_to(:last_name) }
+    it { should respond_to(:password_digest) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
 
-  describe "when password doesn't match confirmation" do
-    before { @staff.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
+    it { should be_valid }
 
-  describe "when password confirmation is nil" do
-    before { @staff.password_confirmation = nil }
-    it { should_not be_valid }
-  end
+    describe "when first name is not present" do
+	before { @staff.first_name = " " }
+	it { should_not be_valid }
+    end
 
-  describe "with a password that's too short" do
-    before { @staff.password = @staff.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
+    describe "when last name is not present" do
+	before { @staff.last_name = " " }
+	it { should_not be_valid }
+    end
 
-  describe "return value of authenticate method" do
-    before { @staff.save }
-    let(:found_staff) { Staff.find_by_email(@staff.email) }
+
+    describe "when first name is too long" do
+	before { @staff.first_name = "a" * 51 }
+	it { should_not be_valid }
+    end
+
+    describe "when last name is too long" do
+	before { @staff.last_name = "a" * 51 }
+	it { should_not be_valid }
+    end
+
+    describe "when user name is already taken" do
+	before do
+	    staff_with_same_user_name = @staff.dup
+	    staff_with_same_user_name.save
+	end
+
+	it { should_not be_valid }
+    end
+
+    describe "when password is not present" do
+	before { @staff.password = @staff.password_confirmation = " " }
+	it { should_not be_valid }
+    end
+
+    describe "when password doesn't match confirmation" do
+	before { @staff.password_confirmation = "mismatch" }
+	it { should_not be_valid }
+    end
+
+    describe "when password confirmation is nil" do
+	before { @staff.password_confirmation = nil }
+	it { should_not be_valid }
+    end
+
+    describe "with a password that's too short" do
+	before { @staff.password = @staff.password_confirmation = "a" * 5 }
+	it { should be_invalid }
+    end
+
+    describe "return value of authenticate method" do
+	before { @staff.save }
+	let(:found_staff) { Staff.find_by_first_name(@staff.first_name) }
+    end
 
     describe "with valid password" do
-      it { should == found_staff.authenticate(@staff.password) }
-    end
+	it { should == found_staff.authenticate(@staff.password) }
+	end
 
     describe "with invalid password" do
-      let(:staff_for_invalid_password) { found_staff.authenticate("invalid") }
+	let(:staff_for_invalid_password) { found_staff.authenticate("invalid") }
 
-      it { should_not == staff_for_invalid_password }
-      specify { staff_for_invalid_password.should be_false }
+	it { should_not == staff_for_invalid_password }
+	specify { staff_for_invalid_password.should be_false }
     end
-  end
 
-  describe "remember token" do
-    before { @staff.save }
-    its(:remember_token) { should_not be_blank }
-  end
+    describe "remember token" do
+	before { @staff.save }
+	its(:remember_token) { should_not be_blank }
+    end
 end
