e019d90f06601132ae6977c7d82f4ee10440f202
Konstantin Devyatov <kdnalus@gmail.com>
2013-01-25 12:40:16 +0000

    fixed hurdles

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,11 @@ reps = User.create!([
   { first_name: "Alex", last_name: "Eldridge", email: "alexeldridge@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "hurdles"},
   { first_name: "Graham", last_name: "MacDonald", email: "gmacdonald95656@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "hurdles"},
   { first_name: "John", last_name: "Stainsby", email: "johnstainsby@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "wattball"},
-  { first_name: "Nikola", last_name: "Damaskov", email: "mamatakolio@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "wattball"}
+  { first_name: "Nikola", last_name: "Damaskov", email: "mamatakolio@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "wattball"},
   { first_name: "Robbie", last_name: "Henderson", email: "robbie.gavin.henderson@gmail.com", address: "Edinburgh", telephone: "+44", password: "123456", password_confirmation: "123456", sport: "hurdle"}
 ]) 
-hurdles = Hurdles.create!([
-  { first_name: "Saad", last_name: "Arif", gender: "m", qualification: "00:00:50", user_id: 2 },
-  { first_name: "", last_name: "", gender: "", qualification: "04:77", user_id: 3 },
-  { first_name: "", last_name: "", gender: "", qualification: "04:77", user_id: 4 },
-  { first_name: "Robbie", last_name: "Henderson", gender: "m", qualification: "00:01:10", user_id: 7 }
-]}
+
+hurdleSaad = User.find(2).hurdles.create!(first_name: "Saad", last_name: "Arif", gender: "m", qualification: "00:00:50")
+hurdleAlex = User.find(3).hurdles.create(first_name: "Alex", last_name: "Eldrige", gender: "m", qualification: "0):01:30")
+ hurdleAlex = User.find(4).hurdles.create(first_name: "Graham", last_name: "MacDonald", gender: "m", qualification: "00:00:00")
+ hurdleAlex = User.find(7).hurdles.create(first_name: "Robbie", last_name: "Henderson", gender: "m", qualification: "00:01:10")
