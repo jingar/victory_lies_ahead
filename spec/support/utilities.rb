fbe47fc4190d9f2c3337a466bf667ef07f1ede28
Konstantin Devyatov <kdnalus@gmail.com>
2013-02-01 19:50:51 +0000

    fixed a typo in utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -17,7 +17,7 @@ def sign_in(user)
 end
 
 def sign_in_staff(staff)
-  visit staffsignin_path
+  visit staff_signin_path
   fill_in "Username",    with: staff.user_name
   fill_in "Password", with: staff.password
   click_button "Sign in"
