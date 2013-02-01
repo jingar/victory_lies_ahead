645dfa339dda3de0920d587dda722eb129a54d64
Saad Arif <saadarif006@gmail.com>
2013-02-01 20:38:42 +0000

    Fixed authentication pages test - all working

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -61,7 +61,7 @@ describe "Authentication" do
 
 	        	describe "after signing in" do
 	        		it "should render the desired protected page" do
-	        			page.should have_selector('title', text: 'Edit user')
+	        			page.should have_selector('title'), text: 'Edit user'
 	        		end
 	        	end
        		 end
