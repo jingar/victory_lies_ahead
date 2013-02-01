2de228c5f39ece745ff80632c163ba07d9a33958
splittie96 <robbie.gavin.henderson@gmail.com>
2013-02-01 13:46:17 +0000

    added pages for wattball team edit, adding assignment security work in progress

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
     def new
 		@team = Team.new
-		11.times { @team.wattballs.build }
+		2.times { @team.wattballs.build }
 		
     end
 
@@ -13,8 +13,22 @@ class TeamsController < ApplicationController
 		@teams = Team.all
     end
 
-    def create
-		@team = current_user.teams.build(params[:team], :without_protection => true)
+    def edit
+    	@team = Team.find(params[:id])
+  	end
+
+  	def update
+		@team = Team.find(params[:id])
+		if @team.update_attributes(params[:team])
+			flash[:success] = "Team updated"
+			redirect_to @team
+		else
+			render 'edit'
+		end
+	end
+
+    def create	
+		@team = current_user.teams.build(params[:team])
 		if @team.save
 		    flash[:success] = "Wattball team is now registred!"
 		    redirect_to @team
