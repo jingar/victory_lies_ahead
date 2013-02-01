f3db4c624489549b41df1f77cbac27b58575e7f4
Konstantin Devyatov <kdnalus@gmail.com>
2013-02-01 13:06:11 +0000

    changed the name of match_teams table to fix annotate, added empty heat model

--- spec/models/heat_spec.rb
+++ spec/models/heat_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: heats
+#
+#  id         :integer          not null, primary key
+#  when       :datetime
+#  gender     :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Heat do
