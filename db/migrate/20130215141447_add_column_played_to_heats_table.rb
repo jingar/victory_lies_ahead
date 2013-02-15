dcd8bfa6a35343234b5a707eb9df12b444d1f54f
Konstantin Devyatov <kdnalus@gmail.com>
2013-02-15 14:18:57 +0000

    migrated db to add played in heats table

--- db/migrate/20130215141447_add_column_played_to_heats_table.rb
+++ db/migrate/20130215141447_add_column_played_to_heats_table.rb
@@ -1,6 +1,6 @@
 class AddColumnPlayedToHeatsTable < ActiveRecord::Migration
   def up
-    add_column :heats, :played, :boolean, :default => true
+    add_column :heats, :played, :boolean, :default => false
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215122806) do
+ActiveRecord::Schema.define(:version => 20130215141447) do
 
   create_table "heat_hurdles", :force => true do |t|
     t.integer  "heat_id"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(:version => 20130215122806) do
 
   create_table "heats", :force => true do |t|
     t.string   "gender"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.datetime "time"
     t.integer  "round"
     t.integer  "tournament_id"
+    t.boolean  "played",        :default => false
   end
 
   create_table "hurdles", :force => true do |t|
