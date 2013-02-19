45d395bd8bcd17f9ca2dae458a10ab7e20c6d718
Saad Arif <saadarif006@gmail.com>
2013-02-19 13:12:14 +0000

    Admin Team Edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,18 +8,18 @@ module ApplicationHelper
 		end
 	end
 
-	def link_to_add_player(name, f, association)
-    	new_object = f.object.class.reflect_on_association(association).klass.new
-    	player = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-    		render(association.to_s.singularize + "_fields", :f => builder)
-    	end
-  	link_to_function(name, "add_player(this, \"#{association}\", \"#{escape_javascript(player)}\")")
-  	end
-
-  	def link_to_remove_player(name, f)
-    	f.hidden_field(:_destroy) + link_to_function(name, "remove_player(this)")
-  	end
-
+  def link_to_add_player(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    player = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_player(this, \"#{association}\", \"#{escape_javascript(player)}\")")
+  end
+  
+  def link_to_remove_player(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_player(this)")
+  end
+  
   def sortable(column,model,title = nil)
     title ||= column.titleize
     direction = column == sort_column(model) && sort_direction == "asc" ? "desc" : "asc"
