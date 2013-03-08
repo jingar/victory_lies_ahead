38402e08642c53e445c3168b875011a21dceeec6
splittie96 <robbie.gavin.henderson@gmail.com>
2013-03-08 15:11:35 +0000

    tickets done on standard page again

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,20 @@
+class TicketsController < ApplicationController
+  def new
+  	@ticket = Ticket.new
+	end
+
+  def index
+    @tickets = Ticket.order(sort_column(Ticket) + " " + sort_direction)
+  end
+
+	def create
+		@ticket = Ticket.new(params[:ticket])
+		if @ticket.save
+      TicketMailer.ticket_email(@ticket).deliver
+      flash[:success] = "You have successfully bought a ticket"
+      redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+end
