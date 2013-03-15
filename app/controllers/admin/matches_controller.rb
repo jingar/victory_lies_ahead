0c75e284a4d0ad80b1ac48da262a9e88976dcf6b
unknown <gmacdonald95656@gmail.com>
2013-03-15 14:49:03 +0000

    Auto schedule wattball working

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -1,8 +1,38 @@
 class Admin::MatchesController < Admin::AdminBaseController
+ include ActiveModel::MassAssignmentSecurity
+
+  attr_accessible :awayGoals, :awayTeam, :homeGoals, :homeTeam, :pitch, :umpire, :when
+
+require 'rrschedule'
+
   # GET /matches
   # GET /matches.json
   def index
     @matches = Match.all
+    
+    @teams = Team.all(:select => "team_name")
+    
+    #creating the array of teams to be used for automatic scheduling
+    array = Array.new
+	@teams.each do |t|
+		array.push t.team_name
+	end
+
+  @schedule=RRSchedule::Schedule.new(
+  #array of teams that will compete against each other. If you group teams into multiple flights (divisions),
+  #a separate round-robin is generated in each of them but the "physical constraints" are shared
+  :teams => [array],
+
+  #Setup some scheduling rules
+  :rules => [
+    RRSchedule::Rule.new(:wday => 0, :gt => ["12:00PM", "7:45PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 1, :gt => ["12:00PM", "7:45PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 2, :gt => ["12:00PM", "7:45PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 3, :gt => ["12:00PM", "7:45PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 4, :gt => ["12:00PM", "7:45PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 5, :gt => ["15:00PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+    RRSchedule::Rule.new(:wday => 6, :gt => ["1:00PM"], :ps => ["1", "2", "3", "4", "5", "6", "7", "8"]),
+  ],
 
     respond_to do |format|
       format.html # index.html.erb
