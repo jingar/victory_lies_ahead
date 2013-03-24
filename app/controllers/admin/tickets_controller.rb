9ac001eaa8cee0003fe8c40c94c4b46dcd7eb52d
Saad Arif <saadarif006@gmail.com>
2013-03-24 21:40:06 +0000

    Fixed ticket controller

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,24 +1,23 @@
 class Admin::TicketsController < Admin::AdminBaseController
-  	def new
-  	@ticket = Ticket.new
-	end
-
-        def index
-          @tickets2 = Ticket.all
-          if !@tickets2
-            @tickets2.each do |t|
-                @adult = t.adult_tickets_bought
-                @concession = t.concession_tickets_bought
-                @total = (@adult * 5) + (@concession * 3)
-              end
-            end
-          else
-            @adult = 0
-            @concession = 0
-            @total = 0
-          end
-          @tickets = Ticket.search(params[:search])
-        end
+  def new
+    @ticket = Ticket.new
+  end
+  
+  def index
+    @tickets2 = Ticket.all
+    if !@tickets2
+      @tickets2.each do |t|
+        @adult = t.adult_tickets_bought
+        @concession = t.concession_tickets_bought
+        @total = (@adult * 5) + (@concession * 3)
+      end
+    else
+          @adult = 0
+          @concession = 0
+          @total = 0
+    end
+        @tickets = Ticket.search(params[:search])
+  end
 
 	def create
           @ticket = Ticket.new(params[:ticket])
